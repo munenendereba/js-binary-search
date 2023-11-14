98d744a498ea3e27068f23ce1c7f0cd32164e89d
Isaac Munene Ndereba <38168136+munenendereba@users.noreply.github.com>
2023-11-14 15:50:57 +0300

    recurse countdown

---
+++ countdownRecursive.js
@@ -0,0 +1,22 @@
+function countDownIterative(count) {
+  while (count > 0) {
+    console.log(count);
+    count--;
+  }
+  console.log("Blast Off~!");
+}
+
+console.log("Iterative Countdown:");
+countDownIterative(3);
+
+function countDownRecursive(count) {
+  if (count <= 0) {
+    console.log("Blast Off~!");
+    return;
+  }
+  console.log(count);
+  countDownRecursive(count - 1);
+}
+
+console.log("Recursive Countdown:");
+countDownRecursive(3);
